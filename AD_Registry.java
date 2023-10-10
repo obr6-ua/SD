fa74474053a0b49af480cd19e7750260049ee8f9
obr6-ua <78750367+obr6-ua@users.noreply.github.com>
2023-10-10 16:35:50 +0000

    sdad

--- AD_Registry.java
+++ AD_Registry.java
@@ -1,113 +1,49 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
-import java.util.Scanner;
 
-public class AD_Registry {
-    private int puertoEscucha;
-    private Map<String, DatosDron> registroDrones;
+public class AD_Registry{
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.err.println("Uso: java Server <puerto>");
+            System.exit(1);
+        }
 
-    public AD_Registry(int puertoEscucha) {
-        this.puertoEscucha = puertoEscucha;
-        this.registroDrones = new HashMap<>();
-    }
+        int portNumber = Integer.parseInt(args[0]);
 
-    public String registrarDron(DatosDron datosDron) {
-        String token = generarToken();
-        registroDrones.put(token, datosDron);
-        return token;
-    }
+        try {
+            // Crear un socket de servidor en el puerto especificado
+            ServerSocket serverSocket = new ServerSocket(portNumber);
+            System.out.println("El servidor está escuchando en el puerto " + portNumber);
 
-    private String generarToken() {
-        return UUID.randomUUID().toString();
-    }
-
-    public void iniciarServidor() {
-        try (ServerSocket socketServidor = new ServerSocket(puertoEscucha)) {
-            System.out.println("AD_Registry está escuchando en el puerto " + puertoEscucha + "...");
             while (true) {
-                Socket socketCliente = socketServidor.accept();
-                System.out.println("Conexión entrante desde " + socketCliente.getInetAddress());
+                // Esperar a que un cliente se conecte
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Cliente conectado desde " + clientSocket.getInetAddress());
 
-                Scanner entrada = new Scanner(socketCliente.getInputStream());
-                PrintWriter salida = new PrintWriter(socketCliente.getOutputStream(), true);
+                // Crear un lector de entrada para recibir datos del cliente
+                
 
-                String solicitud = entrada.nextLine();
-                System.out.println("Solicitud recibida: " + solicitud);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-                // Analizar la solicitud como JSON
-                // Suponemos que la solicitud tiene el formato {"action": "register", "drone_info": {...}}
-                // Debes adaptar este código de análisis según tu protocolo de comunicación real
-                Map<String, Object> solicitudMapa = analizarJson(solicitud);
+                // Crear un escritor de salida para enviar datos al cliente
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
 
-                if (solicitudMapa != null && solicitudMapa.containsKey("action") && solicitudMapa.containsKey("drone_info")) {
-                    String accion = (String) solicitudMapa.get("action");
-                    if ("register".equals(accion)) {
-                        DatosDron datosDron = (DatosDron) solicitudMapa.get("drone_info");
-                        String token = registrarDron(datosDron);
+                // Leer el mensaje del cliente
+                String mensajeCliente = in.readLine();
+                System.out.println("Mensaje recibido del cliente: " + mensajeCliente);
 
-                        // Enviar el token de vuelta al dron
-                        Map<String, Object> respuestaMapa = new HashMap<>();
-                        respuestaMapa.put("estado", "éxito");
-                        respuestaMapa.put("token", token);
-                        String respuesta = convertirAJson(respuestaMapa);
-                        salida.println(respuesta);
-                    }
-                }
+                // Responder al cliente con "Hola"
+                out.println("Hola");
 
-                socketCliente.close();
+                // Cerrar la conexión con el cliente
+                clientSocket.close();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
-    // Método auxiliar para analizar una cadena JSON en un mapa
-    private Map<String, Object> analizarJson(String json) {
-        // Puedes utilizar una biblioteca JSON como Jackson o Gson para un análisis más robusto
-        // Este es un enfoque simplificado para fines de demostración
-        // Adapta este código según el formato JSON que utilices
-        try {
-            return new ObjectMapper().readValue(json, new TypeReference<Map<String, Object>>() {});
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    // Método auxiliar para convertir un objeto a cadena JSON
-    private String convertirAJson(Object obj) {
-        // Puedes utilizar una biblioteca JSON como Jackson o Gson para una serialización más robusta
-        // Este es un enfoque simplificado para fines de demostración
-        // Adapta este código según el formato JSON que utilices
-        try {
-            return new ObjectMapper().writeValueAsString(obj);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public static void main(String[] args) {
-        int puertoRegistro = 9000;
-        AD_Registry adRegistry = new AD_Registry(puertoRegistro);
-        adRegistry.iniciarServidor();
-    }
-}
-
-// Define una clase para representar los datos de un dron
-class DatosDron {
-    private String id;
-    private String alias;
-
-    public DatosDron(String id, String alias) {
-        this.id = id;
-        this.alias = alias;
-    }
-
-    // Getters y setters para id y alias
 }
