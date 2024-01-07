b921a6b7daa0387b64c3014e50a5d1b6ea0efa53
obr6-ua <obr6@alu.ua.es>
2024-01-07 14:13:03 +0100

    funciona

--- AWD_Python/run_drones.sh
+++ AWD_Python/run_drones.sh
@@ -4,11 +4,16 @@
 ID=1
 PORT=4001
 
-# Ejecutar el comando 8 veces
+# Ejecutar el comando 8 veces en terminales separadas
 for i in {1..8}
 do
-    echo "Ejecutando docker-compose para ID=$ID y puerto $PORT:4000"
-    docker-compose run -e ID=$ID -p $PORT:4000 drone
+    # Comando a ejecutar en la nueva terminal
+    COMMAND="echo 'Ejecutando docker-compose para ID=$ID y puerto $PORT:4000'; docker-compose run -e ID=$ID -p $PORT:4000 drone"
+
+    # Abrir una nueva terminal y ejecutar el comando
+    gnome-terminal -- /bin/bash -c "$COMMAND"
+    
+    # Incrementar ID y PORT
     ((ID++))
     ((PORT++))
 done
