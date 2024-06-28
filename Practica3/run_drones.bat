cc960a0c6701dfcb3e908d802853566cf2bbe8c3
obr6-ua <78750367+obr6-ua@users.noreply.github.com>
2024-06-28 15:27:47 +0000

    cambios

--- Practica3/run_drones.bat
+++ Practica3/run_drones.bat
@@ -2,13 +2,19 @@
 setlocal EnableDelayedExpansion
 
 REM Inicializar ID y puerto
-set /a ID=1
-set /a PORT=4001
+set ID=1
+set PORT=4001
+set API=N
 
 REM Ejecutar el comando 8 veces en ventanas separadas
 FOR /L %%i IN (1,1,8) DO (
-    start cmd /k "echo Ejecutando docker-compose para ID=!ID! y puerto !PORT!:4000 && docker-compose run -e ID=!ID! -p !PORT!:4000 drone && exit"
-    set /a ID=!ID!+1
-    set /a PORT=!PORT!+1
+    REM Cambiar API a S después de las primeras 4 iteraciones
+    @REM if %%i GEQ 5 (
+    @REM     set API=S
+    @REM )
+
+    start cmd /k "echo Ejecutando docker-compose para ID=!ID! y puerto !PORT!:4000 && docker-compose run -e ID=!ID! -e API=!API! -p !PORT!:4000 drone && exit"
+    set /a ID+=1
+    set /a PORT+=1
 )
 endlocal
